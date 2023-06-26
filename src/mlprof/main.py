bcdc21d1db195b9c7c6d5bf602f42d5ac2aa36ee
Sam Foreman <saforem2@gmail.com>
2023-06-26 17:06:25 -0500

    Update `main.py`

--- src/mlprof/main.py
+++ src/mlprof/main.py
@@ -1,61 +1,55 @@
 """
 main.py
 
-Conains simple implementation illustrating how to use PyTorch DDP for
-distributed data parallel training.
+Main entry point for `mlprof`
 """
 from __future__ import absolute_import, annotations, division, print_function
+import sys
 
 import hydra
-import sys
-# from hydra.utils import instantiate
-# import numpy as np
 from omegaconf import DictConfig
 import wandb
 
-from mlprof.utils.pylogger import get_pylogger
+from mlprof import get_logger
+import mlprof.utils.dist as dist
 from mlprof.utils.dist import cleanup, setup_torch
-
-import deepspeed.comm as dist
-import warnings
-
-
-warnings.filterwarnings('ignore')
-
-
-log = get_pylogger(__name__)
+log = get_logger(__name__)
 
 
 def train_mnist(cfg: DictConfig) -> float:
     from mlprof.trainers.pytorch.trainer import Trainer
     trainer = Trainer(config=cfg)
     _ = trainer.train()
-    test_acc = trainer.test()
-    return test_acc
+    return trainer.test()
 
 
 def run(cfg: DictConfig) -> float:
-    _ = setup_torch(
+    rank = setup_torch(
         seed=cfg.get('seed', 12345),
         precision=cfg.get('precision', 'fp32'),
         backend=cfg.backend,
         port=cfg.get('port', '2345')
     )
-
-    test_acc = train_mnist(cfg)
-    return test_acc
+    if rank == 0:
+        if cfg.get('wandb', None) is not None:
+            from mlprof.common import setup_wandb
+            setup_wandb(cfg)
+    return train_mnist(cfg)
 
 
 # @record
 @hydra.main(version_base=None, config_path='./conf', config_name='config')
 def main(cfg: DictConfig) -> None:
     run(cfg)
-    if str(cfg.get('backend', '')).lower() == 'ddp':
+    if wandb.run is not None:
+        wandb.run.save('.', policy='end')
+    if (backend := str(cfg.get('backend', '')).lower()) == 'ddp':
         cleanup()
+    if backend in {'deepspeed', 'ds'}:
+        import deepspeed.comm as dscomm
+        dscomm.log_summary()
 
 
 if __name__ == '__main__':
-    wandb.require('service')
     main()
     sys.exit(0)
-    # dist.log_summary()
